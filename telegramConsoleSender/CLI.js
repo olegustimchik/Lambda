eb3656c804a628190c3e60c5ce32f5f823e82810
olegustimchik <olegustimchik84@gmail.com>
2023-08-23 23:29:21 +0300

    improve bot work. Add setInterval() which reads user_input.json every 0.5 seconds

--- telegramConsoleSender/CLI.js
+++ telegramConsoleSender/CLI.js
@@ -1,15 +1,18 @@
 const { program } = require('commander');
-const {ConsoleSenderBot} = require('./console-sender-bot.js');
-
-const consoleSenderBot = new ConsoleSenderBot();
+const fs = require('fs');
 
 console.time("Time :");
 
 program.command("message <msg>").alias("m").description("Send written message to the bot").action((msg) => {
-    consoleSenderBot.sendMessage(msg);
+    fs.writeFile("./user_input.json", JSON.stringify({ "message": msg }), (err) => {
+        console.log(err);
+    });
 });
+
 program.command("photo <path>").alias("p").description("Send a photo to the bot. Please write the valid path of your image. You can also drag and drop the image to the console").action((path) => {
-    consoleSenderBot.sendImage(path);
+    fs.writeFile("./user_input.json", JSON.stringify({ "path": path }), (err) => {
+        console.log(err);
+    });
 });
 
 
--- telegramConsoleSender/console-sender-bot.js
+++ telegramConsoleSender/console-sender-bot.js
@@ -1,40 +1,64 @@
 const TelegramBot = require('node-telegram-bot-api')
 const fs = require('fs');
 const mime = require('mime-types');
-
 require('dotenv').config();
+
 const getImageTitle = function (str) {
     return str.split("\\").pop().split("/").pop().split(".")[0];
 }
 
-class ConsoleSenderBot {
-    constructor() {
-        this.bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
-    }
+const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 
-    sendMessage(message) {
-        this.bot.sendMessage(process.env.CHAT_ID, message).catch((error) => {
-            console.error("Some error occurred while sending message: " + error.code);
-            console.error(error);
-        });
-        this.bot.stopPolling();
-    }
+const message = "bot started";
 
-    sendImage(imgLocation) {
-        let fileName = getImageTitle(imgLocation) || "filename";
-        let contentType = 'application/octet-stream';
-        if (process.env.NTBA_FIX_350) {
-            contentType = mime.lookup(fileName) || 'application/octet-stream';
-        }
-        this.bot.sendPhoto(process.env.CHAT_ID, imgLocation, {}, { filename: fileName, contentType: contentType }).catch((error) => {
-
-            console.error("Some error occurred while sending photo: " + error.code);
-        });
-        this.bot.stopPolling();
+function sendImage(imgLocation) {
+    let fileName = getImageTitle(imgLocation) || "filename";
+    let contentType = 'application/octet-stream';
+    if (process.env.NTBA_FIX_350) {
+        contentType = mime.lookup(fileName) || 'application/octet-stream';
     }
+    bot.sendPhoto(process.env.CHAT_ID, imgLocation, {}, { filename: fileName, contentType: contentType }).catch((error) => {
+        console.error("Some error occurred while sending photo: " + error.code);
+    });
 }
 
+function sendMessage(message) {
+    bot.sendMessage(process.env.CHAT_ID, message).catch((error) => {
+        console.error("Some error occurred while sending message: " + error.code);
+    });
+    bot.stopPolling();
+}
 
-module.exports = { ConsoleSenderBot };
+function writeEmptyString() {
+    fs.writeFile("./user_input.json", "", (err) => {
+        if (err) {
+            console.log(err.message);
+        }
+    });
+}
 
+const timerId = setInterval(() => {
+    fs.readFile("./user_input.json", "utf8", (err, data) => {
+        if (err) {
+            console.log("some error occurred");
+            console.log(err.message);
+        }
+
+        if (data !== "") {
+            if (JSON.parse(data).message) {
+                sendMessage(JSON.parse(data).message);
+            } else {
+                sendImage(JSON.parse(data).path);
+            }
+            writeEmptyString();
+        }
+    });
+    console.log(process.memoryUsage().heapUsed / Math.pow(10, 6));
+}, 500);
+
+setTimeout(() => {
+    clearInterval(timerId);
+    console.log('stop');
+    bot.stopPolling();
+}, 100000);
 
