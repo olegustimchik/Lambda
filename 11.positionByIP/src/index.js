1bb35844e664612d134cdfbb3244dec285c8ae47
olegustimchik <71694504+olegustimchik@users.noreply.github.com>
2023-12-11 23:05:26 +0200

    remove 11.positionByIP/searchRequestIP/request-ip.js. Now all propertys, where can ip stored, reads in router callback

--- 11.positionByIP/src/index.js
+++ 11.positionByIP/src/index.js
@@ -1,9 +1,7 @@
-import express from 'express';
-import fs from 'fs';
-import getIP from '../searchRequestIP/request-ip.js';
-import convertIpToDecimal from '../converter/convertIpToDecimal.js';
+import express from "express";
+import convertIpToDecimal from './converter/convertIpToDecimal.js';
 import readIPs from '../resources/readIPs.js';
-import binarySearch from './binarySearch.js';
+import binarySearch from './search/binarySearch.js';
 
 const port = 3000;
 const app = express();
@@ -14,29 +12,41 @@ const ranges = await readIPs().then((res) => {
         console.log(err);
     }
 });
-
+app.set('trust proxy', true);
 app.get('/', (req, res) => {
     try {
-        let respObject = new Object(); 
-        let clientIp = getIP(req); 
-        let convertedIP = convertIpToDecimal(clientIp); 
-        let range = binarySearch(ranges, convertedIP); 
-        if (convertedIP && clientIp && range){ 
-            respObject.yourIP = clientIp; 
-            respObject.location = range.country; 
-            respObject.range = range; 
-        } else { 
-            respObject.errorMassage = "error occurred while trying to convert your ip"; 
-            respObject.yourIP = clientIp; 
-            respObject.range = range; 
+        let clientIp = req.ip;
+        let convertedIP = convertIpToDecimal(clientIp);
+        let range;
+        if (clientIp && convertedIP) {
+            range = binarySearch(ranges, convertedIP);
+            console.log(`client IP(req.ip): ${clientIp}`);
+            return res.send({ "IP": clientIp, "countryCode":range.countryCode,"country":range.country });
         }
-        console.log(respObject);
-        res.send(respObject);
+
+        clientIp = req.socket.remoteAddress;
+        convertedIP = convertIpToDecimal(clientIp);
+        if (clientIp && convertedIP) {
+            range = binarySearch(ranges, convertedIP);
+            console.log(`client IP(req.socket.remoteAddress): ${clientIp}`);
+            return res.send({ "IP": clientIp, "countryCode":range.countryCode,"country":range.country });
+        }
+
+        clientIp = req.rawHeaders[req.rawHeaders.findIndex((elem) => elem === "X-Forwarded-For") + 1];
+        convertedIP = convertIpToDecimal(clientIp);
+        if (clientIp && convertedIP) {
+            range = binarySearch(ranges, convertedIP);
+            console.log(`client IP(req.rawHeaders): ${clientIp}`);
+            return res.send({ "IP": clientIp, "countryCode":range.countryCode,"country":range.country });
+        }
+        res.status(500).json({"error": "can't get your ip"});
     } catch (e) {
-        console.log({"error" : e.code, "message" : e.message});
-        res.send({"error" : e.code, "message" : e.message});
+        console.log({ "error": e.code, "message": e.message });
+        res.sendStatus(500);
     }
 });
 
-app.listen(port);
+app.listen(port, () => {
+    console.log(`Express listening on port ${port}`);
+});
 
